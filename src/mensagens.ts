629f33ba1a6e22c9ff1eabbda2db21a17e6950e6
Ana Júlia Rubim <anajrbcosta25@gmail.com>
2025-09-18 22:26:09 -0300

    update

--- src/mensagens.ts
+++ src/mensagens.ts
@@ -7,8 +7,8 @@ export default class Mensagens {
 
     public comoUsar = () => {
         console.log(`Para efetuar os cálculos digite os números e a opção de cálculo desejados. Separe-os por espaço. Exemplo: "4 7 Somar"`)
-        console.log(`Se uma função: "1 -5 6 Func" > "1x² -5x + 6"`)
-        console.log(`O resultado será a soma dos números: 4`)
+        console.log(`Se for uma função: "2 -4 8 Func" > "1x² -4x + 8"`)
+        console.log(`O resultado será a soma dos números: 11`)
         console.log(`Para finalizar digite "Sair"\n`)
     }
 
